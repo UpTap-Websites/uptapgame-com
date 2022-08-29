6857b09911fb988a97bf2a1f664a13e0b386098a
Tai Zeng <zengtai@gmail.com>
2022-08-29 18:38:41 +0800

    忘了加引用代码了

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+import { ADSENSE_ID } from "../lib/constants";
+
 class MyDocument extends Document {
   // static async getInitialProps(ctx) {
   //   const initialProps = await Document.getInitialProps(ctx);
@@ -9,7 +11,13 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script
+            async
+            src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${ADSENSE_ID}`}
+            crossOrigin="anonymous"
+          ></script>
+        </Head>
         <body className="bg-slate-600">
           <Main />
           <NextScript />
