783838ddd422e5700671f4d5643629731f32aa78
Tai Zeng <zengtai@gmail.com>
2023-06-20 15:12:20 +0800

    推送一下

--- components/ScrollTopButton.js
+++ components/ScrollTopButton.js
@@ -1,9 +1,6 @@
 export default function ScrollTopButton({ scrollToTop, showScrollTop }) {
   return (
-    <button
-      onClick={scrollToTop}
-      className={`scroll-top-btn ${showScrollTop ? `grid` : `hidden`}`}
-    >
+    <button onClick={scrollToTop} className={`scroll-top-btn ${showScrollTop ? `grid` : `hidden`}`}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
@@ -12,11 +9,7 @@ export default function ScrollTopButton({ scrollToTop, showScrollTop }) {
         stroke="currentColor"
         className="h-6 w-6"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
-        />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
       </svg>
     </button>
   );
