d0007aa6af136e74303bf9e884b58dbf86c73e1a
Tai Zeng <zengtai@gmail.com>
2024-03-27 17:58:22 +0800

    修改搜索部分的api

--- hooks/useGameData.js
+++ hooks/useGameData.js
@@ -4,7 +4,7 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function useGameData() {
   // const url = `${process.env.NEXT_PUBLIC_API_URL}/graphql`;
-  const url = `${process.env.NEXT_PUBLIC_API_V2_URL}/items/game?fields=appid,title,slug,category&limit=-1`;
+  const url = `${process.env.NEXT_PUBLIC_API_V2_URL}/items/game?fields=appid,title,slug,category.name,category.slug&limit=-1`;
 
   const { data, error, isLoading } = useSWR(url, fetcher);
 
